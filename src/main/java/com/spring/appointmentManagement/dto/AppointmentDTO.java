495861ce728e820304bc893a7843830c2fdd2be3
Aatinfo <aathifinfo115@gmail.com>
2023-08-09 21:39:10 +0530

    after validating appointment dto

--- src/main/java/com/spring/appointmentManagement/dto/AppointmentDTO.java
+++ src/main/java/com/spring/appointmentManagement/dto/AppointmentDTO.java
@@ -1,12 +1,24 @@
 package com.spring.appointmentManagement.dto;
 
+import jakarta.validation.constraints.*;
+import org.checkerframework.checker.optional.qual.Present;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class AppointmentDTO {
 
     private long appointmentID;
+    @NotBlank(message = "The appointment type is required")
+    @Size(min = 3, max = 20, message = "The appointment type must be from 3 to 20 characters")
     private String appointmentType;
+
+    @NotNull(message = "The date is required")
+    @Future(message = "Appointment date must be in the future")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
+
+    @NotNull(message = "The amount is required")
     private double amount;
 
 
